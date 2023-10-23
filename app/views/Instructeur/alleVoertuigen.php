6c311af1f704cb512df742e9d3af823f279b0256
Shahed Amer <chanyeol6521354@gmail.com>
2023-10-23 23:33:20 +0200

    final commit

--- app/views/Instructeur/alleVoertuigen.php
+++ app/views/Instructeur/alleVoertuigen.php
@@ -1,15 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?= URLROOT; ?>/css/style.css">
     <title>Document</title>
 </head>
-<body>
-<h3><u><?= $data['title']; ?></u></h3>
 
-<table>
+<body>
+    <h3><u><?= $data['title']; ?></u></h3>
+
+    <table>
         <thead>
             <th>Type Voertuig</th>
             <th>Type</th>
@@ -24,10 +26,11 @@
             <?= $data['tableRows']; ?>
         </tbody>
     </table>
-    
+    ssssssssssssssssssssssssssssssssssssssss
     <?php if (isset($data['deleteMessage'])) : ?>
         <div><?= $data['deleteMessage']; ?></div>
     <?php endif ?>
-    
+
 </body>
+
 </html>
